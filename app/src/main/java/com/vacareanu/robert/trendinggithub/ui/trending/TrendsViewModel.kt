f047eca09fe210d37685838c49149502e2f8c5ce
Robert Vacareanu <robert.vacareanu@gmail.com>
2017-11-23 23:26:12 +0200

    Livedata merge

--- app/src/main/java/com/vacareanu/robert/trendinggithub/ui/trending/TrendsViewModel.kt
+++ app/src/main/java/com/vacareanu/robert/trendinggithub/ui/trending/TrendsViewModel.kt
@@ -6,6 +6,7 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.persistence.room.Room
+import android.util.Log
 import com.vacareanu.robert.trendinggithub.db.AppDatabase
 import com.vacareanu.robert.trendinggithub.model.Repository
 import com.vacareanu.robert.trendinggithub.network.ApiResponse
@@ -24,15 +25,33 @@ class TrendsViewModel(application: Application) : AndroidViewModel(application) 
 
     // Responsible for merging data in db and data from network
     init {
+        Log.v("TVM", "Init")
         roomRepositories = Room.databaseBuilder(application.applicationContext, AppDatabase::class.java, "database-name").build().repositoryDao().findAll()
         networkRepositories = transformationFromApiResponseToList(
                 GithubService.githubNetwork.create(GithubService::class.java)
                         .getRepositories("language=kotlin+created%3A>${SimpleDateFormat("yyyy-MM-dd")
                                 .format(Date().time - 14 * 24 * 60 * 60 * 1000)}", "stars")
         )
+        roomRepositories.value?.forEach {
+            Log.v("TVM", "${it.isFavorite}, ${it.url}")
+        }
         repositories.addSource(networkRepositories) { t: List<Repository>? ->
+            t?.let {
+                t
+                        .filter { r -> roomRepositories.value?.firstOrNull { r.url == it.url } != null }
+                        .forEach { it.isFavorite = true }
+            }
             repositories.value = t
         }
+        repositories.addSource(roomRepositories) { t: List<Repository>? ->
+            t?.let {
+                t.forEach { r -> r.isFavorite=true
+                    networkRepositories.value?.firstOrNull { r.url == it.url }?.let { it.isFavorite = true }
+
+                }
+            }
+        }
+
 
     }
 
